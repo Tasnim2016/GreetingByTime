28b7c3389b4997a284da556da27016359b794c5c
tasnimrahman <tasnim.rahman@bs-23.com>
2018-04-24 12:22:36 +0600

    Initial

---
+++ index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+
+module.exports = function() {
+    var dt = new Date();
+            var hours=dt.getHours();
+            if (hours < 12)
+            {
+                console.log("Good Morning!");
+
+            }
+            else if(hours==12)
+            {
+                console.log("Good Noon!");
+            }
+            else if (hours> 12 && hours <17) {
+                console.log("Good Afternoon!");
+            }
+            else if (hours > 17 && hours < 19) {
+                console.log("Good Evening!");
+            }
+            else {
+                console.log("Good Night!");
+            }
+}
